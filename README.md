1a7752b5ebe29128a3de278c5bb44371cae1c0b1
Berkay <Berkay@berkay-net.local>
2018-01-10 00:32:53 +0300

    sequential link capacity estimation using Packet Train Probing Technique

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SDNWirelessLinkCapacityEstimator
 Wireless link capacity estimation using RYU contoller
+
+## Start Controller: 
+ryu-manager main.py (use "--verbose" option for additional debug)
+## Start two switch topology: 
+sudo mn topo=linear,2 --controller=remote 
+## Check switch flow tables:
+sudo ovs-ofctl dump-flows s1 (s1 switch id)
+Note: please check topo using mininet command "net"
+
--- main.py
+++ main.py
@@ -13,30 +13,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# coding: utf-8
+
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
-from ryu.lib.packet import icmp
 from ryu.lib.packet import ethernet
 
 # for sending periodic probe packets
-import thread
+import threading
 import time
 
-UNIQUE_PROBE_PACKET_ID = 0x8888
+PROBE_PACKET_ID_START = 37400
+PROBE_PACKET_ID_END = 37440 
 
-class ExampleSwitch13(app_manager.RyuApp):
+class LinkCapacityEstimator(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(ExampleSwitch13, self).__init__(*args, **kwargs)
+        super(LinkCapacityEstimator, self).__init__(*args, **kwargs)
         # initialize mac address table.
         self.mac_to_port = {}
         self.is_sent_already = False
-        self.current_eth_type = UNIQUE_PROBE_PACKET_ID
+        self.datapath_1 = None
+        self.measure_start_time = 0
+        
+
+        # start mesauring sequence
+        threading.Thread(target=self.auto_send_probe_packet).start()
+
+    
+    def auto_send_probe_packet(self):
+        while True:
+            time.sleep(10)
+            self.logger.info("Initializing measuring sequence")
+            self.measure_start_time = time.time()
+            if self.datapath_1 is not None:
+                eth_type_will_send = PROBE_PACKET_ID_START
+                while(eth_type_will_send <= PROBE_PACKET_ID_END):
+                    self._send_probe_packet(self.datapath_1, self.datapath_1.ofproto.OFPP_FLOOD, eth_type_will_send)
+                    #self.logger.info("Packet Sent datapathid: " + str(self.datapath_1.id) + "\n")
+                    eth_type_will_send += 1
+            else:
+                self.logger.info("Datapath is not initialized!")
+
 
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -47,14 +71,18 @@ class ExampleSwitch13(app_manager.RyuApp):
 
         # install the table-miss flow entry.
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
-        self.add_flow(datapath, 0, match, actions)
-
-        """
-        match = parser.OFPMatch(eth_type=UNIQUE_PROBE_PACKET_ID)
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 1, match, actions)
-        """
+        
+        
+        match1 = parser.OFPMatch(eth_type=PROBE_PACKET_ID_START)
+        match2 = parser.OFPMatch(eth_type=PROBE_PACKET_ID_END)
+
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 0, match1, actions)
+        self.add_flow(datapath, 0, match2, actions)
+
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
@@ -74,13 +102,17 @@ class ExampleSwitch13(app_manager.RyuApp):
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-         
+
         # print packet info
         pkt = packet.Packet(data=msg.data)
         
         # get Datapath ID to identify OpenFlow switches.
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
+
+        # assing the local datapath variable
+        if dpid == 1:
+            self.datapath_1 = datapath
 
         # analyse the received packets using the packet library.
         pkt = packet.Packet(msg.data)
@@ -106,26 +138,19 @@ class ExampleSwitch13(app_manager.RyuApp):
         # construct action list.
         actions = [parser.OFPActionOutput(out_port)]
 
-        """
+        
         # install a flow to avoid packet_in next time.
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
             self.add_flow(datapath, 1, match, actions)
 
-        print("Datapath: "+ str(datapath) + " bufferid: " + str(ofproto.OFP_NO_BUFFER) + " in_port: " + str(in_port) + 
-                 " actions: " + str(actions) + " data" + str(msg.data))
-        """
-
-        pkt_icmp = pkt.get_protocol(icmp.icmp)
-        if pkt_icmp: #and not self.is_sent_already:
-            self._send_probe_packet(datapath, ofproto.OFPP_FLOOD)
-            self.logger.info("Packet Sent Inport: " + str(in_port) + "datapathid: " + str(dpid) + "\n") 
-
-            #self.is_sent_already = True
-
-        if eth_pkt.ethertype >= UNIQUE_PROBE_PACKET_ID:
-            self.logger.info("packet-in %s" % (pkt,))
-            self.logger.info("Packet received Inport: " + str(in_port) + "datapathid: " + str(dpid) + "\n") 
+        if eth_pkt.ethertype == PROBE_PACKET_ID_START:
+            self.logger.info("packet-in data %s" % (pkt,))
+        if eth_pkt.ethertype == PROBE_PACKET_ID_END:
+            self.logger.info("packet-in data %s" % (pkt,))
+            self.logger.info("Start Time : " + str(self.measure_start_time))
+            self.logger.info("End Time : " + str(time.time()))
+            self.logger.info("Link Capacity: " + str((49*60)/(time.time() - self.measure_start_time)))
 
         # construct packet_out message and send it.
         out = parser.OFPPacketOut(datapath=datapath,
@@ -136,21 +161,18 @@ class ExampleSwitch13(app_manager.RyuApp):
         datapath.send_msg(out)
 
     
-    def _send_probe_packet(self, datapath, port):
+    def _send_probe_packet(self, datapath, port, eth_type):
 
         pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(ethertype=self.current_eth_type,
+        pkt.add_protocol(ethernet.ethernet(ethertype=eth_type,
                                            dst="00:00:00:00:00:01",
                                            src="00:00:00:00:00:02"))
-        self.current_eth_type += 1
         self._send_packet(datapath, port, pkt)
 
     def _send_packet(self, datapath, port, pkt):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         pkt.serialize()
-        self.logger.info("packet-out %s" % (pkt,))
-
         data = pkt.data
         actions = [parser.OFPActionOutput(port=port)]
         out = parser.OFPPacketOut(datapath=datapath,
